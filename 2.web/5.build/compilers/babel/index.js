db697799781f26105eac02c9795052cc990959b7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-03-21 21:50:46 -0300

    feat: try babel

---
+++ 2.web/5.build/compilers/babel/index.js
@@ -0,0 +1,17 @@
+import capitalize from './src/capitalize.js';
+import deepClone from './src/deepClone.js';
+import formatDate from './src/formatDate.js';
+import isPrime from './src/isPrime.js';
+import randomInRange from './src/randomInRange.js';
+import square from './src/square.js';
+
+function main() {
+    console.log(capitalize('hello')); // Hello
+    console.log(deepClone({ a: 1, b: { c: 2 } })); // { a: 1, b: { c: 2 } }
+    console.log(formatDate(new Date('2020-01-01'), 'YYYY/MM/DD')); // 2020/01/01
+    console.log(isPrime(7)); // true
+    console.log(randomInRange(1, 10)); // 1 ~ 10
+    console.log(square(2)); // 4
+}
+
+main();
---
+++ 2.web/5.build/compilers/babel/src/square.js
@@ -0,0 +1,5 @@
+const square = (n) => {
+    return n * n;
+};
+
+export default square;
